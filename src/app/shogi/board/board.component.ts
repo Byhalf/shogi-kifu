4ab8329e23c16f93cd2654ebd1238c2371cdf558
gareth <gareth.lafay@gmail.com>
2025-03-20 20:29:44 +0100

    made shogi pieces draggable/droppable

--- src/app/shogi/board/board.component.ts
+++ src/app/shogi/board/board.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { TileComponent } from '../tile/tile.component';
-import { Tile } from '../interfaces/tile';
-import {INITIAL_SHOGI_BOARD, Koma} from '../interfaces/koma';
+import {TileComponent} from '../tile/tile.component';
+import {Tile} from '../interfaces/tile';
+import {INITIAL_SHOGI_BOARD} from '../interfaces/koma';
 
 @Component({
   selector: 'shogi-board',
@@ -11,13 +11,49 @@ import {INITIAL_SHOGI_BOARD, Koma} from '../interfaces/koma';
   styleUrl: './board.component.css',
 })
 export class BoardComponent {
-  readonly boardTiles: Array<Tile> = [];
+  boardTiles: Tile[][] = [];
+
   readonly width: number = 9;
   constructor() {
-    for (let x: number = 0; x < this.width; x++) {
-      for (let y: number = 0; y < this.width; y++) {
-        this.boardTiles[x + y * this.width] = { x: x, y: y, koma: INITIAL_SHOGI_BOARD[y][x] };
+    for (let y: number = 0; y < this.width; y++) {
+      this.boardTiles[y] = []; // Initialize each row as an array
+      for (let x: number = 0; x < this.width; x++) {
+        this.boardTiles[y][x]  = { x: x, y: y, koma: INITIAL_SHOGI_BOARD[y][x] };
       }
     }
+  }
+
+  selectedTile: Tile|undefined
+  targetedTile: Tile|undefined;
+
+  handleTileDrop(tile: Tile): void {
+    console.log('Tile dropped:', tile);
+    this.targetedTile = tile;
+    if (this.selectedTile) {
+      this.updateBoard(this.selectedTile, this.targetedTile);
+    }
+  }
+
+  handleTileUnselect(tile: Tile) {
+    console.log('Tile unselected :', tile);
+    this.selectedTile = undefined;
+  }
+
+  handleTileSelect(tile: Tile) {
+    console.log('Tile selected:', tile);
+    this.selectedTile = tile;
+  }
+
+  updateBoard(selectedTile : Tile,targetedTile: Tile) {
+    console.log('Update board');
+
+    this.boardTiles[selectedTile.y][selectedTile.x] = {x: selectedTile.x, y: selectedTile.y, koma: undefined} ;
+
+    selectedTile.x = targetedTile.x;
+    selectedTile.y = targetedTile.y;
+
+    this.boardTiles[targetedTile.y][targetedTile.x] = selectedTile ;
+
+    //this.boardTiles = [...this.boardTiles]; // Triggers change detection
   }
 }
--- src/app/shogi/tile/tile.component.ts
+++ src/app/shogi/tile/tile.component.ts
@@ -1,20 +1,43 @@
-import { Component, HostListener, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { Tile } from '../interfaces/tile';
 import {Koma, KOMA_SVG_MAP} from '../interfaces/koma';
-
+import {NgIf} from '@angular/common';
+import { EventEmitter } from '@angular/core';
 @Component({
   selector: 'shogi-tile',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './tile.component.html',
   styleUrl: './tile.component.css',
 })
 export class TileComponent {
-  getCoordinates() {
-    console.log(`(${this.tile.x}, ${this.tile.y})`);
-  }
+
   @Input() tile: Tile = { x: 0, y: 0 };
+  @Output() tileDropped  = new EventEmitter<Tile>();
+  @Output() tileSelected  = new EventEmitter<Tile>();
+  @Output() tileUnSelected  = new EventEmitter<Tile>();
+
 
   getSvg(koma: Koma) {
-    return KOMA_SVG_MAP[koma.type + '_'+koma.player];
+    return KOMA_SVG_MAP[koma.kind + '_'+koma.player];
+  }
+
+  onDragStart(event: DragEvent, tile: Tile) {
+    this.tileSelected.emit(tile);
+  }
+
+  onDrop(event: DragEvent, tile: Tile) {
+    event.preventDefault();
+    this.tileDropped.emit(tile);
+  }
+// required for onDrop firing
+  allowDrop(event: DragEvent): void {
+    event.preventDefault();
+  }
+
+  onDragEnd($event: DragEvent) {
+    $event.preventDefault();
+    this.tileUnSelected.emit($event);
   }
 }
