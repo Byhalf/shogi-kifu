fcc6b11f6a0607614d8e2a281c49b1398dbd8732
gareth <gareth.lafay@gmail.com>
2025-04-16 21:15:43 +0200

    feat: img to svg

--- src/app/shogi/modules/tile/tile.component.ts
+++ src/app/shogi/modules/tile/tile.component.ts
@@ -2,11 +2,13 @@ import {Component, Input} from '@angular/core';
 import {Tile} from '../../interfaces/tile';
 import {getSvg} from '../../interfaces/koma';
 import {NgIf} from '@angular/common';
-import {Subject} from 'rxjs';
+import {Subject, Subscription} from 'rxjs';
 import {BoardEventBusServiceService} from '../../services/event-services/board-event-bus-service.service';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'shogi-tile',
+  selector: 'app-shogi-tile',
   imports: [
     NgIf
   ],
@@ -16,16 +18,33 @@ import {BoardEventBusServiceService} from '../../services/event-services/board-e
 export class TileComponent {
 
   @Input() tile: Tile = {x: -1, y: -1};
-
+  safeSvg: SafeHtml = '';
   private drop$ = new Subject<Tile>();
-  boardEventBusService: BoardEventBusServiceService;
+  private httpSubscription: Subscription | undefined;
 
-  constructor(boardEventBusService: BoardEventBusServiceService) {
-    this.boardEventBusService = boardEventBusService;
-
-
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer, private boardEventBusService: BoardEventBusServiceService) {
   }
 
+  ngOnInit() {
+    if (this.tile.koma) {
+      this.httpSubscription = this.http.get(getSvg(this.tile.koma), {responseType: 'text'}).subscribe({
+        next: (svg) => {
+          this.safeSvg = this.sanitizer.bypassSecurityTrustHtml(svg);
+        },
+        error: (error) => {
+          console.error('Error fetching SVG:', error);
+        },
+        complete: () => {
+        }
+      });
+
+    }
+    console.log('helo')
+  }
+
+  ngOnDestroy() {
+    this.httpSubscription?.unsubscribe();
+  }
 
   onDragStart(tile: Tile) {
     this.boardEventBusService.selectTile(tile);
