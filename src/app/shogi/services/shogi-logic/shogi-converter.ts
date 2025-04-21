a457d6a5611c28a20db157d89faa3f0b26195ada
gareth <gareth.lafay@gmail.com>
2025-04-21 18:10:04 +0200

    fix: csa export

--- src/app/shogi/services/shogi-logic/shogi-converter.ts
+++ src/app/shogi/services/shogi-logic/shogi-converter.ts
@@ -2,7 +2,8 @@ import {Koma, LATIN_TO_CSA, PlayerType, promotePiece} from '../../interfaces/kom
 import {Move} from '../../interfaces/move';
 
 export class ShogiConverter {
-  public static boardStateToCSA(state: (Koma | undefined)[][], firstPlayer: PlayerType): string {
+  private static boardStateToCSA(state: (Koma | undefined)[][], firstPlayer: PlayerType): string {
+
     let result = "V2.2\n"
     for (let i = 0; i < state.length; i++) {
       result += "P" + (i + 1);
@@ -22,14 +23,20 @@ export class ShogiConverter {
     return result;
   }
 
-  public static moveToCSA(move: Move, firstPlayer: PlayerType): string {
+  private static moveToCSA(move: Move, firstPlayer: PlayerType): string {
     let result = firstPlayer === move.player ? "+" : "-";
+    const maybeFlip = (x: number, y: number): { x: number; y: number } => {
+      const [fx, fy] = firstPlayer === 'sente' ? [x, y] : [8 - x, 8 - y];
+      return ShogiConverter.virtualCoordinatesToReal(fx, fy);
+    };
+
     result += move.origin ?
-      (({x, y}) => `${x}${y}`)(ShogiConverter.virtualCoordinatesToReal(move.origin.x, move.origin.y)) : "00"
-    result += (({x, y}) => `${x}${y}`)(ShogiConverter.virtualCoordinatesToReal(move.destination.x, move.destination.y));
+      (({x, y}) => `${x}${y}`)(maybeFlip(move.origin.x, move.origin.y)) : "00"
+    result += (({x, y}) => `${x}${y}`)(maybeFlip(move.destination.x, move.destination.y));
     result += move.promotion === "*" ? LATIN_TO_CSA[promotePiece(move.koma)] : LATIN_TO_CSA[move.koma];
     return result;
   }
+
 
   // shogi boards are japanese x is left to right also index starts at 1
   public static virtualCoordinatesToReal(x: number, y: number): { x: number, y: number } {
@@ -41,8 +48,8 @@ export class ShogiConverter {
       return ShogiConverter.boardStateToCSA(state, "sente") + "\n+";
     }
     const firstPlayer = moves[0].player;
-    let result = "";
-    result += ShogiConverter.boardStateToCSA(state, firstPlayer);
+    let result = ShogiConverter.boardStateToCSA(state, "sente") + "\n+";
+
     result += "\n+\n";
     result = moves.reduce((result, move) => {
       return result + ShogiConverter.moveToCSA(move, firstPlayer) + "\n";
